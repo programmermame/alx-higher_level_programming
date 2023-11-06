b3e2c7bd39c4cf9a87df2e33d1e737e154228980
programmermame <mameatpickout@gmail.com>
2023-11-06 21:06:43 +0300

    DataStructure

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *node;
+	int values[9999], i = 0, c = 0;
+
+	if ((!*head) || (!head))
+	{
+		return (1);
+	}
+	node = *head;
+	if (!node->next)
+	{
+		return (1);
+	}
+	while (node)
+	{
+		values[i] = node->n;
+		node = node->next;
+		i++;
+	}
+	i--;
+	while (i >= 0 && c <= i)
+	{
+		if (values[i] != values[c])
+		{
+			return (0);
+		}
+		i--;
+		c++;
+	}
+	return (1);
+}
