7cef370709698d5e2934595cda7d5deb9999e75d
programmermame <mameatpickout@gmail.com>
2023-11-06 21:51:24 +0300

    DataStructure

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if(my_list is not None):
-        m_list = my_list.copy()
-        if idx < 0 or idx >= len(my_list):
-            return my_list
-        else:
-            m_list[idx] = element
-            return m_list
+    my_list = my_list[:]
+    if idx >= 0 and idx < len(my_list):
+        my_list[idx] = element
+    return my_list
