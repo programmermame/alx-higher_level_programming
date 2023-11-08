fa3b951f68e522c7b9bca352d0ad8c6533bc5093
programmermame <mameatpickout@gmail.com>
2023-11-08 17:38:34 +0300

    More Data Structures: Set, Dictionary

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     my_list = my_list[:]
-    if idx >= 0 and idx < len(my_list):
+    if 0 <= idx < len(my_list):
         my_list[idx] = element
     return my_list
+
+
+
